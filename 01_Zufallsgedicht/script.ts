efff95ef125c2f9ec4548a55eac88c15741a8e56
Nellyeyy <80837378+Nellyeyy@users.noreply.github.com>
2023-03-28 12:46:43 +0200

    Update Script.ts

--- 01_Zufallsgedicht/script.ts
+++ 01_Zufallsgedicht/script.ts
@@ -3,11 +3,24 @@ namespace poem {
     let verbs: string[]=["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
     let objects: string[]=["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
 
-console.log(subjects);
-console.log(verbs);
-console.log(objects); 
-for (let i = subjects.length; i > 0; i--) {
-    console.log(i);
 
-  }
+for (let index: number = subjects.length; index > 0; index--) {  
+  let results: string = getVers(subjects, verbs, objects);
+  console.log(results);
+  
+}
+
+
+  function getVers (_subjects: string[], _verbs: string[], _objects: string[]): string {
+
+    let randomnr1: number = Math.floor(Math.random() * _subjects.length);
+    let randomnr2: number = Math.floor(Math.random() * _verbs.length);
+    let randomnr3: number = Math.floor(Math.random() * _objects.length);
+    let vers: string = " ";
+    vers = vers + _subjects.splice(randomnr1, 1)[0] + " ";
+    vers = vers + _verbs.splice(randomnr2, 1)[0] + " ";
+    vers = vers + _objects.splice(randomnr3, 1)[0] + " ";
+   
+    return vers;
+}
 }
